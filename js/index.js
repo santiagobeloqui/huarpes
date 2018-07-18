8b5fd11a9ec1c615cf817addc6c9ea943ba46e96
Santiago <sabeloqui@gmail.com>
2018-07-18 16:06:26 -0300

    Header adaptado a mobile

--- js/index.js
+++ js/index.js
@@ -5,6 +5,10 @@ let posSectionPortadaBottom = sectionPortada.getBoundingClientRect().bottom;
 //----- Adaptación Header ------
 let headerLogo = document.getElementById("header-logo");
 let header = document.getElementById("header");
+let logoMobile = document.getElementById("logo-mobile");
+let menuButtonPart = document.getElementsByClassName("menu-button-part");
+let headerMobile = document.getElementById("header-mobile");
+
 
 
 //----- Animación Nav Responsive-----
@@ -27,10 +31,20 @@ window.onscroll = ()=>{
         headerLogo.style.opacity = 1;
         header.classList.add("header-white");
         nav.classList.add("nav-color-grey");
+        logoMobile.classList.add("logo-mobile-visible");
+        for(let part of menuButtonPart){
+            part.classList.add("menu-button-part-orange");
+        }
+        headerMobile.classList.add("header-mobile-visible");
     }
     if (window.pageYOffset < posSectionPortadaBottom){
         headerLogo.style.opacity = 0;
         header.classList.remove("header-white");
         nav.classList.remove("nav-color-grey");
+        logoMobile.classList.remove("logo-mobile-visible");
+        for(let part of menuButtonPart){
+            part.classList.remove("menu-button-part-orange");
+        }
+        headerMobile.classList.remove("header-mobile-visible");
     }
 };
